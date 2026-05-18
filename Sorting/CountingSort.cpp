50dc7f3723212af8623a1c75fe4a7bc8b46fa5c1
michelangelodem <micn@demiridis.gr>
2026-05-18 19:20:53 +0300

    Polished abstractions in counting sort.

--- Sorting/CountingSort.cpp
+++ Sorting/CountingSort.cpp
@@ -5,63 +5,55 @@ void CountingSort::sort(vector<Record>& records) {
         cout << "Warning: Empty records vector." << endl;
         return;
     }
-
-    long long maxValue = records[0].getCumulative()/1000; // Scale down to handle large values;
-    if (maxValue < 0) {
-            printf("Error: In ");
-            records[0].display();
-            printf(" - ");
-            printf("Negative value encountered: %lld\n", maxValue);
-        throw invalid_argument("CountingSort cannot handle negative values for the selected field.");
-    }
-
-    if (maxValue > SIZE_MAX) {
-        printf("Error: In ");
-        records[0].display();
-        printf(" - ");
-        printf("Value exceeds maximum allowed size: %lld\n", maxValue);
-        throw invalid_argument("CountingSort cannot handle values that exceed the maximum allowed size.");
-    }
-
-    for (const Record& record : records) {
-        long long value = record.getCumulative()/1000; // Scale down to handle large values
-        if (value < 0) {
-            printf("Error: In ");
-            record.display();
-            printf(" - ");
-            printf("Negative value encountered: %lld\n", value);
-            throw invalid_argument("CountingSort cannot handle negative values for the selected field.");
-        }
-    if (value > SIZE_MAX) {
-        printf("Error: In ");
-        record.display();
-        printf(" - ");
-        printf("Value exceeds maximum allowed size: %lld\n", value);
-        throw invalid_argument("CountingSort cannot handle values that exceed the maximum allowed size.");
-    }        
-        if (value > maxValue) {
-            maxValue = value;
-        }
-    }
-
-    vector<long long> count(static_cast<size_t>(maxValue) + 1, 0);
-    for (const Record& record : records) {
-        long long value = record.getCumulative()/1000; // Scale down to handle large values
-        count[value]++;
-    }
-
-    for (long long i = 1; i <= maxValue; ++i) {
-        count[i] += count[i - 1];
-    }
-
+    
+    long long maxValue = setMaxValue(records);
+    vector<long long> count = getFrequencyArr(records, maxValue);
     vector<Record> output(records.size());
+    
+    long long value = 0;
+    long long position = 0;
     for (long long i = records.size() - 1; i >= 0; --i) {
-        long long value = records[i].getCumulative()/1000;
-        output[count[value] - 1] = records[i];
+        value = records[i].getCumulative()/1000;
+        position = count[value] -1;
+        output[position] = records[i];
         count[value]--;
     }
-
+    
     for (size_t i = 0; i < records.size(); ++i) {
         records[i] = output[i];
     }
+}
+
+long long CountingSort::setMaxValue(vector<Record>& records) {
+    long long maxValue = records[0].getCumulative() / 1000;
+    if ((maxValue < 0)||(maxValue > SIZE_MAX)) 
+        throw invalid_argument("Invalid value at records index 0");
+    
+    long long value = 0;
+    for (const auto& record : records) {
+        value = record.getCumulative() / 1000;
+        if ((value < 0)||(value > SIZE_MAX)) 
+            throw invalid_argument("Invalid value while determining max value");
+        
+        if (value > maxValue)
+            maxValue = value;
+    }
+
+    return maxValue;
+}
+
+vector<long long> CountingSort::getFrequencyArr(vector<Record>& records, long long maxValue) {
+    vector<long long> count(static_cast<size_t>(maxValue) + 1, 0);
+    //count:
+    long long value = 0;
+    for (const auto& record : records) {
+        value = record.getCumulative() / 1000;
+        count[value]++;
+    }
+
+    //add the previous:
+    for (long long i = 1; i <= maxValue; ++i) {
+        count[i] += count[i-1];
+    }
+    return count;
 }
